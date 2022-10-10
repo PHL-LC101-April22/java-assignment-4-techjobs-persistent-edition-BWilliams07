6627c398ca641ece56daa828d4a9f0619b14f81d
BWilliams07 <104173554+BWilliams07@users.noreply.github.com>
2022-10-10 16:11:03 -0400

    changes

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,50 +1,32 @@
 package org.launchcode.techjobs.persistent.models;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("unused")
 @Entity
 public class Skill extends AbstractEntity {
 
-
     @ManyToMany(mappedBy = "skills")
-    private List<Job> jobs = new ArrayList<>();
-    @NotBlank(message = "description can not be blank")
-    @Size(max=200)
+    private final List<Job> jobs= new ArrayList<>();
+
+    public Skill(){}
+
+
+    @NotNull
+    @NotBlank
+    @Size(min=3, max = 600)
     private String description;
 
-    public Skill( @NotBlank(message = "description can not be blank") @Size(max=200) String description) {
-        this.description = description;
-    }
-
-    public Skill() {}
-
-    public String getDescription() {
+    public String getDescription(){
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setDescription(String description){
+        this.description=description;
     }
-
     public List<Job> getJobs() {
         return jobs;
-    }
-
-    public void setJobs(List<Job> jobs) {
-        this.jobs = jobs;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
     }
 }
