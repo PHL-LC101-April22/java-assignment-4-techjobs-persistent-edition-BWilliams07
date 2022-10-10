be313b8f726e7348ea5dc3ba9f4dcb222d17a7f4
BWilliams07 <104173554+BWilliams07@users.noreply.github.com>
2022-10-10 16:13:13 -0400

    changes

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Skill extends AbstractEntity {
 
     @ManyToMany(mappedBy = "skills")
-    private final List<Job> jobs= new ArrayList<>();
+    private List<Job> jobs= new ArrayList<>();
 
     public Skill(){}
 
